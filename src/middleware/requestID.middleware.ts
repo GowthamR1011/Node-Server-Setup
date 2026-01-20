206a711bae940a035ee10387b9f1fd3794d8fb8f
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2026-01-20 09:22:16 +0000

    feat: Updated to use the crypto package instead of UUID package for generating random request IDs.

--- src/middleware/requestID.middleware.ts
+++ src/middleware/requestID.middleware.ts
@@ -1,9 +1,9 @@
-import { v4 } from "uuid";
+import { randomUUID } from "crypto";
 import { Request, Response, NextFunction } from "express";
 
 function requestIdAssigner(req: Request, res: Response, next: NextFunction) {
   const incomingId = req.header("x-request-id");
-  const requestId = incomingId || v4();
+  const requestId = incomingId || randomUUID();
 
   req.requestId = requestId;
   res.setHeader("X-Request-ID", requestId);
